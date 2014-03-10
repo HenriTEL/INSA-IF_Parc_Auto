5dad3d964b191af0c2177617e8389dcefc684000
HenriTEL <hhannetel@gmail.com>
2014-03-10 17:58:50 +0100

    gestion du canal

--- Sortie.cpp
+++ Sortie.cpp
@@ -26,7 +26,7 @@
 //---------------------------------------------------- Variables statiques
 
 //------------------------------------------------------ Fonctions privées
-static void usr2_handler(int signo)
+static void usr2_handler( int signo )
 // Mode d'emploi :
 //
 // Contrat :
@@ -34,10 +34,37 @@ static void usr2_handler(int signo)
 // Algorithme :
 //
 {
+/*
+	extern vector<pid_t> pid_voiturier;
 	if( signo == SIGUSR2 )
+	{
+		for( int i=0; i < pid_voiturier.size(); i++ )
+			kill( pid_voiturier[i], SIGUSR2 );
 		exit( EXIT_SUCCESS );
+	}
+*/
 } //----- fin de sig_handler
 
+static void chld_handler( int signo )
+// Mode d'emploi :
+//
+// Contrat :
+//
+// Algorithme :
+//
+{
+	pid_t pid;
+	int status;
+
+	if( signo == SIGCHLD )
+	{
+		pid = wait( *status );
+		// virer voiture dans mem partagée
+		// décrémenter nbplaces
+		signal(SIGCHLD, chld_handler);
+	}
+		//exit( EXIT_SUCCESS );
+} //----- fin de sig_handler
 //////////////////////////////////////////////////////////////////  PUBLIC
 //---------------------------------------------------- Fonctions publiques
 
@@ -46,23 +73,35 @@ pid_t VoiturierSortie ()
 // Algorithme :
 //
 {
-	pid_t pid;
+	pid_t pid_sortie;
+/*	extern int pipe_end[2]; // obtenu à partir de la tâche simulation */
+/*	vector<pid_t> pid_voiturier; */
+	char place;
 
 	signal(SIGUSR2, usr2_handler);
-	if( (pid = fork()) == 0 ) // fork is successful
+	if( (pid_sortie = fork()) == 0 )
+	// Fils
 	{
 		// tableau de pid des voituriers - SortirVoiture() -
 		// Bloquer signaux
 		// Mettre en place pipe
+		signal( SIGCHLD, chld_handler );
 		for( ;; )
 		{
-			pause();
+/*
+			close( pipe_end[1] ); // Fermeture de la sortie du canal
+			while ( read( pipe_end[0], &place, 1 ) > 0 )
+			// Une voiture veut sortir
+			{
+				pid_voiturier.append( SortirVoiture( atoi(place) ) );
+			}
+*/
 			// si SIGUSR2 kill + exit
 			// /!\ (in/dé)crémenter au bon moment nb voitures
 			// SortirVoiture
 			// /!\ délais entre chaque voitur pour sortir
 		}
 	}
-	return pid;	
+	return pid_sortie;
 } //----- fin de VoiturierSortie
 
